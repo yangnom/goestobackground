40fc94c405ac24a8a2c0783b738c4a07052879e2
Yangnom <ynom@pm.me>
2021-06-28 16:01:15 -0700

    Initial Commit

---
+++ Shared/ObjectWillChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectWillChangeApp.swift
+//  Shared
+//
+//  Created by ynom on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ObjectWillChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
