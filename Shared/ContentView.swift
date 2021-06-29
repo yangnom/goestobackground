b5acbf1738a096869dcfd51dec62f8f5197f7b16
Yangnom <ynom@pm.me>
2021-06-28 23:04:53 -0700

    Tabs.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,18 +1,37 @@
 //
 //  ContentView.swift
-//  Shared
+//  ObjectWillChange
 //
 //  Created by ynom on 6/28/21.
 //
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @SceneStorage("selectedView") var selectedView: String?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedView) {
+            NotificationMethodView()
+                .tag(NotificationMethodView.tag)
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            ScenePhaseMethod()
+                .tag(ScenePhaseMethod.tag)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            
+       }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Shared/ScenePhaseMethod.swift
@@ -0,0 +1,22 @@
+//
+//  ScenePhaseMethod.swift
+//  ObjectWillChange
+//
+//  Created by ynom on 6/28/21.
+//
+
+import SwiftUI
+
+struct ScenePhaseMethod: View {
+    static let tag: String? = "scene"
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScenePhaseMethod_Previews: PreviewProvider {
+    static var previews: some View {
+        ScenePhaseMethod()
+    }
+}
